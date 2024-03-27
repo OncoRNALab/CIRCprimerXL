d05e86d4f3bf02ddd40b4b35cdc6bbc527d09c2f
MariekeVromman <74721287+MariekeVromman@users.noreply.github.com>
2024-03-27 13:57:46 +0100

    improve gtf_to_bed script to deal with 'chr'

--- bin/gtf_to_bed.py
+++ bin/gtf_to_bed.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+# WARNING: this works for gtf file downloaded from ensembl. It does not work well for gtf files downloaded from Gencode.
+
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -12,13 +14,19 @@ gtf = open(args.i[0])
 bed = open(args.o[0], 'w')
 
 for line in gtf:
-	chrom, havana, annotation, start, end, point, strand, point2, info = line.split('\t')
 
-	if annotation == "exon":
-		gene_id, gene_version, transcript_id, transcript_version, exon_number, skip = info.split(';', 5)
-		
-		transcript_id = transcript_id.lstrip().rstrip().replace('transcript_id ', '').replace('"', '')
-		transcript_version = transcript_version.lstrip().rstrip().replace('transcript_version ', '').replace('"', '')
-		exon_number = exon_number.lstrip().rstrip().replace('exon_number ', '').replace('"', '')
+	if line[0] != '#': # skip header
 
-		bed.write("chr" + chrom + '\t' + str(int(start) - 1 ) + '\t' + end + '\t' + transcript_id + '_' + transcript_version + '_exon_' + exon_number + '\n')
+		chrom, havana, annotation, start, end, point, strand, point2, info = line.split('\t')
+
+		if annotation == "exon":
+			gene_id, gene_version, transcript_id, transcript_version, exon_number, skip = info.split(';', 5)
+			
+			transcript_id = transcript_id.lstrip().rstrip().replace('transcript_id ', '').replace('"', '')
+			transcript_version = transcript_version.lstrip().rstrip().replace('transcript_version ', '').replace('"', '')
+			exon_number = exon_number.lstrip().rstrip().replace('exon_number ', '').replace('"', '')
+
+			if chrom[0:3] == "chr": # if the gtf file already contains the chr 
+				bed.write(chrom + '\t' + str(int(start) - 1 ) + '\t' + end + '\t' + transcript_id + '_' + transcript_version + '_exon_' + exon_number + '\n')
+			else: # if the gtf file does not yet contain the chr
+				bed.write("chr" + chrom + '\t' + str(int(start) - 1 ) + '\t' + end + '\t' + transcript_id + '_' + transcript_version + '_exon_' + exon_number + '\n')
